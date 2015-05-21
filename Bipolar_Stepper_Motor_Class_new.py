32d013ada0adc0b4156ed0250b18614cc5d83916
Jason Johnson <jpajohnson@gmail.com>
2015-05-21 11:21:28 -0500

    Create Bipolar_Stepper_Motor_Class_new.py

---
+++ Bipolar_Stepper_Motor_Class_new.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import time
+
+#sequence for a1, b2, a2, b1
+#phase_seq=[[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]];
+#sequence for a, b (not gate/hex inverter) drive
+phase_seq=[[1,0],[0,0],[0,1],[1,1]];
+#full step sequence. maximum torque
+#phase_seq=[[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
+#half-step sequence. double resolution. But the torque of the stepper motor is not constant 
+num_phase=len(phase_seq);
+
+class Bipolar_Stepper_Motor:
+    
+    phase=0;
+    dirction=0;
+    position=0;
+    
+    a=0;#pin numbers
+    b=0;
+    enable=0;
+    def __init__(self,a,b,enable=0):
+    #initial a Bipolar_Stepper_Moter objects by assigning the pins
+    
+        GPIO.setmode(GPIO.BCM);
+        
+        self.a=a;
+        self.b=b;
+        self.enable=enable;
+        
+        GPIO.setup(self.a,GPIO.OUT);
+        GPIO.setup(self.b,GPIO.OUT);
+        #GPIO.setup(self.enable,GPIO.IN);
+        
+        self.phase=0;
+        self.dirction=0;        
+        self.position=0;
+        
+    def move(self, dirction, steps, delay=0.009):
+        #if(GPIO.gpio_function(self.enable) == GPIO.OUT)
+        #    GPIO.setup(self.enable,GPIO.IN);
+        for _ in range(steps):
+            next_phase=(self.phase+dirction) % num_phase;
+            
+            GPIO.output(self.a,phase_seq[next_phase][0]);
+            GPIO.output(self.b,phase_seq[next_phase][1]);
+            
+            self.phase=next_phase;
+            self.dirction=dirction;
+            self.position+=dirction;
+            
+            time.sleep(delay);
+
+    def unhold(self):
+        self.phase=0;
+        #GPIO.setup(self.enable, GPIO.OUT, initial=GPIO.LOW);
+        
