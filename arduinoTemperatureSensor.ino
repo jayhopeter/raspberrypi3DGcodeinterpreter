294b61905e6d31b55ec8a642e852e6ba604e7092
Jason Johnson <jpajohnson@gmail.com>
2015-06-07 10:04:18 -0500

    Update arduinoTemperatureSensor.ino

--- arduinoTemperatureSensor.ino
+++ arduinoTemperatureSensor.ino
@@ -1,9 +1,9 @@
-
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int ctr = 1;
 int sensortotal=0;
+int maxTemp = 450;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -19,40 +19,40 @@ void loop() {
   
   sensortotal += sensorValue;
   //Serial.println(sensortotal);
-  if( ctr % 7 == 0)
+  if( ctr % 10 == 0)
   {
     
-  sensorValue = sensortotal/7;
+  sensorValue = sensortotal/10;
   //Serial.println(sensorValue);
-  if(sensorValue <= 480)
+  if(sensorValue <= maxTemp-20)
   {
     analogWrite(ledPin,0);
   }
-  else if(sensorValue > 480 && sensorValue <= 483)
+  else if(sensorValue > maxTemp-20 && sensorValue <= maxTemp-17)
   {
-    analogWrite(ledPin,20);
+    analogWrite(ledPin,30);
   }
-  else if(sensorValue > 483 && sensorValue <= 486)
-  {
-    analogWrite(ledPin,35);
-  }
-  else if(sensorValue > 486 && sensorValue <= 489)
+  else if(sensorValue > maxTemp-17 && sensorValue <= maxTemp-14)
   {
     analogWrite(ledPin,60);
   }
-  else if(sensorValue > 489 && sensorValue <= 492)
+  else if(sensorValue > maxTemp-14 && sensorValue <= maxTemp-11)
   {
-    analogWrite(ledPin,110);
+    analogWrite(ledPin,100);
   }
-  else if(sensorValue > 492 && sensorValue <= 495)
+  else if(sensorValue > maxTemp-11 && sensorValue <= maxTemp-8)
   {
-    analogWrite(ledPin,160);
+    analogWrite(ledPin,125);
   }
-  else if(sensorValue > 495 && sensorValue<= 500)
+  else if(sensorValue > maxTemp-8 && sensorValue <= maxTemp-5)
+  {
+    analogWrite(ledPin,170);
+  }
+  else if(sensorValue > maxTemp-5 && sensorValue<= maxTemp)
   {
     analogWrite(ledPin,205);
   }
-  else if(sensorValue > 500)
+  else if(sensorValue > maxTemp)
   {
     analogWrite(ledPin,250);
   }
@@ -61,5 +61,5 @@ void loop() {
   ctr = 0;
   }
   ctr += 1;
-  delay(25);                  
+  delay(20);                  
 }
